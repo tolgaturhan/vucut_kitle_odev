39270937602f53670a42278f8d287e7116270249
Tolga Turhan <tolgaturhan@live.com>
2022-10-22 13:00:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        double kilo, boy, sonuc;
+        Scanner indeks = new Scanner(System.in);
+
+        System.out.print("Lütfen Kilonuzu Giriniz: ");
+
+        if (indeks.hasNextDouble()) {
+            kilo = indeks.nextDouble();
+            System.out.print("Lütfen Boyunuzu (m) Giriniz: ");
+
+            if (indeks.hasNextDouble()) {
+                boy = indeks.nextDouble();
+                System.out.println("Hesaplama Yapılıyor Lütfen Bekleyiniz...");
+                sonuc = kilo / (boy * boy);
+                double D = sonuc;
+                float sonucf = (float) D;
+                System.out.println("Vücut Kitle Endeksiniz: " + sonucf);
+            } else {
+                System.out.println("Bu Alana Yalnızca Sayı Girebilirsiniz.!");
+            }
+        } else {
+            System.out.println("Bu Alana Yalnızca Sayı Girebilirsiniz.!");
+        }
+    }
+}
